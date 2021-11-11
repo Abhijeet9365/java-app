6a4487ec2a6520819b0618f27ac69dd9b02c6436
Abhijeet9365 <sheelvantabhijeet46@gmail.com>
2021-11-11 13:47:56 +0530

    "updatd project"

--- BTC-Project/src/com/btc/app/dao/UserDaoJdbcImpl.java
+++ BTC-Project/src/com/btc/app/dao/UserDaoJdbcImpl.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -16,13 +17,13 @@ import com.btc.app.ui.UserAppUI;
 
 
 public class UserDaoJdbcImpl implements UserDao {
-	
+
 	private Connection con;
-	
+
 	private PreparedStatement smt;
 	public Scanner scanner=new Scanner(System.in);
-	
-	
+
+
 	public UserDaoJdbcImpl() throws SQLException {
 		con=ConnectionUtil.getDbConnection();
 	}
@@ -36,24 +37,24 @@ public class UserDaoJdbcImpl implements UserDao {
 		c1.next();
 		int count=c1.getInt(1);
 		System.out.println("Total"+count);
-		
+
 		if(count>0)
 		{
 			throw new InvalidIdException("Data already present");
 		}
-		
+
 		//new code ends
-		
+
 		String query = "insert into user values(?,?,?,?)";
-		
-		
+
+
 		smt= con.prepareStatement(query);
-		
+
 		smt.setInt(1, user.getUserid());
 		smt.setString(2, user.getUsername());
 		smt.setString(3, user.getPassword());
 		smt.setString(4, user.getFavCategory());
-		
+
 		int insertedRowCount = smt.executeUpdate();
 		if(insertedRowCount>0) {
 			return user;
@@ -77,16 +78,16 @@ public class UserDaoJdbcImpl implements UserDao {
 			throw new InvalidIdException("User Id doesnot exist");
 		}
 		//ends
-		
+
 		String query="select * from user where userId=?";
 		smt=con.prepareStatement(query);
-		
+
 		smt.setInt(1, userId);	
-		
+
 		ResultSet queryResult = smt.executeQuery();
-		
+
 		User user=null;
-		
+
 		if(queryResult.next()) {
 			user=new User();
 			user.setUserid(userId);
@@ -94,13 +95,213 @@ public class UserDaoJdbcImpl implements UserDao {
 			user.setPassword(queryResult.getString("password"));
 			user.setFavCategory(queryResult.getString("FavtCategory"));
 		}
-		
-		System.out.println(queryResult.getString("FavtCategory"));
-		
-		return user;
-		
-	
+
+		String favCat=queryResult.getString("FavtCategory");
+		System.out.print("Favorite category="+queryResult.getString("FavtCategory"));
+
+		//new code
+		while(true)
+		{
+			int option;
+			System.out.println();
+			System.out.println("Enter option");
+			System.out.println("1.FAvorite article\t 2.All article\t 3.exit");
+			option=scanner.nextInt();
+			switch(option)
+			{
+			case 1:System.out.println("Favorite category");
+			//favCategory();
+			System.out.println(favCat);
+
+			//newcode
+
+
+			try{
+				String queryfav="select title from news where category=?";
+				smt=con.prepareStatement(queryfav);
+				smt.setString(1,favCat);
+				ResultSet rs=smt.executeQuery();
+				System.out.println("News Title");
+				// HashMap<String>
+				List<String> newsBTC=new ArrayList<String>();
+				while(rs.next()){
+
+					System.out.println(rs.getString("title")+"\t");
+					newsBTC.add(rs.getString("title"));
+				}
+				int num=1;
+				while(num!=0)
+				{
+					//newsBTC.forEach(title->System.out.println(title));
+					System.out.println("Enter news to read");
+					String desc=scanner.next();
+					scanner.nextLine();
+					//whether title exist or not
+
+					String querytitle="select count(*) from news where title=?";
+					smt=con.prepareStatement(querytitle);
+					smt.setString(1,desc);
+					ResultSet queryTitle=smt.executeQuery();
+					queryTitle.next();
+					int countTitle=queryTitle.getInt(1);
+					//System.out.println("Total"+count);
+					if(countTitle<1)
+					{
+						System.out.println("News title doesnot exist");
+						//throw new InvalidIdException("Content doesn't exist");
+					}
+
+
+
+
+
+					String descquery="select content from news where title=?";
+					smt=con.prepareStatement(descquery);
+					smt.setString(1,desc);
+					ResultSet rs1=smt.executeQuery();
+					// System.out.println(rs1);
+					while(rs1.next())
+					{
+						System.out.println(rs1.getString("content")+"\t");
+						System.out.println("Did u Like the content,if(yes) press 1 orelse press 0");
+						int like=scanner.nextInt();
+						if(like==1) {
+							String likeSql = "update News set likecount=likecount+1 where title=?";
+							smt=con.prepareStatement(likeSql);
+							smt.setString(1,desc);
+							smt.executeUpdate();
+							System.out.println("LikeCount updated Succesfully");
+						}
+					} 
+					// close ResultSet rs
+					System.out.println("1.Continue 0.exit");
+					//System.out.println();
+					num=scanner.nextInt();
+					rs1.close();
+				}
+				// rs1.close();
+				rs.close();
+			} catch(SQLException s){						
+				s.printStackTrace();
+			}
+
+
+
+			//return user;
+			break;
+			case 2:System.out.println("Read All news");
+
+			//new code
+			try{
+
+				String queryCat="select category from news group by category";
+				smt=con.prepareStatement(queryCat);
+				//smt.setString(1,favCat);
+				ResultSet rs=smt.executeQuery();
+				// System.out.println("News Title");
+				// HashMap<String>
+				List<String> newsBTC=new ArrayList<String>();
+				while(rs.next()){
+
+					System.out.println(rs.getString("category")+"\t");
+					newsBTC.add(rs.getString("category"));
+				}
+				System.out.println("Please select the category");
+				String selectCat=scanner.next();
+				scanner.nextLine();
+				String queryfav="select title from news where category=?";
+				smt=con.prepareStatement(queryfav);
+				smt.setString(1,selectCat);
+				ResultSet rs1=smt.executeQuery();
+				System.out.println("News Title");
+				// HashMap<String>
+				List<String> newsBTC1=new ArrayList<String>();
+				while(rs1.next()){
+
+					System.out.println(rs1.getString("title")+"\t");
+					newsBTC1.add(rs1.getString("title"));
+				}
+				int num=1;
+				while(num!=0)
+				{
+					//newsBTC.forEach(title->System.out.println(title));
+					System.out.println("Enter news to read");
+					String desc=scanner.next();
+					scanner.nextLine();
+					//whether title exist or not
+
+					String querytitle="select count(*) from news where title=?";
+					smt=con.prepareStatement(querytitle);
+					smt.setString(1,desc);
+					ResultSet queryTitle=smt.executeQuery();
+					queryTitle.next();
+					int countTitle=queryTitle.getInt(1);
+					//System.out.println("Total"+count);
+					if(countTitle<1)
+					{
+						System.out.println("News title doesnot exist");
+						//throw new InvalidIdException("Content doesnot exist");
+					}
+
+
+
+
+
+					String descquery="select content from news where Title=?";
+					smt=con.prepareStatement(descquery);
+					smt.setString(1,desc);
+					ResultSet rs2=smt.executeQuery();
+					// System.out.println(rs1);
+					while(rs2.next())
+					{
+						System.out.println(rs2.getString("content")+"\t");
+						System.out.println("Did u Like the content,if(yes) press 1 orelse press 0");
+						int like=scanner.nextInt();
+						if(like==1) {
+							String likeSql = "update News set likecount=likecount+1 where title=?";
+							smt=con.prepareStatement(likeSql);
+							smt.setString(1,desc);
+							smt.executeUpdate();
+							System.out.println("LikeCount updated Succesfully");
+						}
+					}
+					// close ResultSet rs
+					System.out.println("1.Continue 0.exit");
+					//System.out.println();
+					num=scanner.nextInt();
+					if(num==1) {
+						System.out.println("News title");
+						newsBTC1.forEach((k)->System.out.println(k));
+					}
+
+					rs2.close();
+				}
+				// rs1.close();
+				rs.close();
+			} catch(SQLException s){						
+				s.printStackTrace();
+			}
+
+
+
+
+
+			break;
+			case 3:System.exit(1);
+			break;
+			default:
+				System.out.println("WRong option");
+				//throw new IlleagalArgumentException("Wrong option")
+			}
+		}
+
+
+		//return user;
+
+
+
+
 	}
 
-	
+
 }
